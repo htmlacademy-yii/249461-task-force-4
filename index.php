b4ccb0db197fd469f42cd1139001ef2a5cba23ac
skravtsov <s.kravtsov@hotmail.com>
2022-08-17 21:06:20 +0300

    Камень судьбы. Правки 3.1

--- index.php
+++ index.php
@@ -5,17 +5,19 @@ spl_autoload_register();
 
 $client = 222;
 $executor = 111;
+$request_executor = null;
 
 $newTask = new Task($client);
-
-$newTask->startTask($executor);
 
 assert($newTask->getNextStatus('cancel') === Task::STATUS_CANCELED, 'cancel action');
 assert($newTask->getNextStatus('complete') === Task::STATUS_COMPLETED, 'complete action');
 assert($newTask->getNextStatus('respond') === Task::STATUS_NEW, 'respond action');
 assert($newTask->getNextStatus('refuse') === Task::STATUS_FAILED, 'refuse action');
 
-assert($newTask->getAvailableActions('new', null)[0] === Task::ACTION_RESPOND, 'progress status');
+assert($newTask->getAvailableActions('new', $request_executor)[0] === Task::ACTION_RESPOND, 'new status');
+
+$newTask->startTask($executor);
+
 assert($newTask->getAvailableActions('progress', $executor)[0] === Task::ACTION_REFUSE, 'refuse status');
 assert($newTask->getAvailableActions('progress', $client)[0] === Task::ACTION_COMPLETE, 'complete status');
 assert($newTask->getAvailableActions('new', $client) === [Task::ACTION_CANCEL, Task::ACTION_START], 'respond status');
