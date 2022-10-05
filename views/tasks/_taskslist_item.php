66c173d6988adf7ba0611d3c968bc408f8255f19
skravtsov <s.kravtsov@hotmail.com>
2022-10-05 19:52:43 +0300

    Обработка html тегов в выводе данных

--- views/tasks/_taskslist_item.php
+++ views/tasks/_taskslist_item.php
@@ -1,14 +1,19 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
 <div id="<?= $model->id ?>" class="task-card">
     <div class="header-task">
-        <a href="#" class="link link--block link--big"><?= $model->title ?></a>
-        <p class="price price--task"><?= $model->price ?> ₽</p>
+        <a href="#" class="link link--block link--big"><?= Html::encode($model->title) ?></a>
+        <p class="price price--task"><?= Html::encode($model->price) ?> ₽</p>
     </div>
     <p class="info-text"><span class="current-time">4 часа </span>назад</p>
-    <p class="task-text"><?= $model->description ?>
+    <p class="task-text"><?= Html::encode($model->description) ?>
     </p>
     <div class="footer-task">
-        <p class="info-text town-text"><?= $model->city->name ?></p>
-        <p class="info-text category-text"><?= $model->category->name ?></p>
+        <p class="info-text town-text"><?= Html::encode($model->city->name) ?></p>
+        <p class="info-text category-text"><?= Html::encode($model->category->name) ?></p>
         <a href="#" class="button button--black">Смотреть Задание</a>
     </div>
 </div>
--- views/tasks/index.php
+++ views/tasks/index.php
@@ -1,7 +1,6 @@
 <?php
 
 /** @var yii\web\View $this */
-
 /** @var \app\models\Tasks $model */
 
 use yii\helpers\Html;
